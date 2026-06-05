9f991cd96a0b6a737fe61faf38705d3e5d12bd35
Joy Karmakar <joykarmakar987654321@gmail.com>
2026-06-05 19:11:06 +0530

    cookie cashing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,6 +37,13 @@ export const auth = betterAuth({
     }),
   ],
 
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
+
   trustedOrigins: [ORIGIN],
 
   advanced: {
